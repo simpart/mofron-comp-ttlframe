a68d6cbff830979879ec0bf0c30de41245867932
simpart <simpart>
2018-09-20 11:21:13 +0000

    mod header

--- index.js
+++ index.js
@@ -2,9 +2,9 @@
  * @file   mofron-comp-{@comp-name}/index.js
  * @author simpart
  */
-let mf = require('mofron');
-let Frame = require('mofron-comp-frame');
-let Header = require('mofron-comp-ttlhdr');
+const mf     = require('mofron');
+const Frame  = require('mofron-comp-frame');
+const Header = require('mofron-comp-txtheader');
 
 /**
  * @class mofron.comp.TtlFrame
@@ -21,6 +21,7 @@ mf.comp.TtlFrame = class extends Frame {
         try {
             super();
             this.name('TtlFrame');
+            this.prmMap('text');
             this.prmOpt(po);
         } catch (e) {
             console.error(e.stack);
@@ -33,15 +34,33 @@ mf.comp.TtlFrame = class extends Frame {
      * 
      * @param prm : 
      */
-    initDomConts (prm) {
+    initDomConts () {
         try {
             super.initDomConts();
-            this.addChild(
-                new Header({
-                    title : (undefined !== prm) ? prm : '',
-                    bind  : false
-                })
-            );
+            this.addChild( this.header() );
+        } catch (e) {
+            console.error(e.stack);
+            throw e;
+        }
+    }
+    
+    header (prm) {
+        try {
+            if (undefined === prm) {
+                /* getter */
+                if (undefined === this.m_header) {
+                    this.header(new Header({ bind  : false }));
+                }
+                return this.m_header;
+            }
+            /* setter */
+            if (true !== mf.func.isInclude(prm, 'TxtHeader')) {
+                throw new Error('invalid parameter');
+            }
+            if (undefined !== this.m_header) {
+                this.updChild(this.m_header, prm);
+            }
+            this.m_header = prm;
         } catch (e) {
             console.error(e.stack);
             throw e;
@@ -49,47 +68,45 @@ mf.comp.TtlFrame = class extends Frame {
     }
     
     text (prm) {
-        try {
-            return this.child()[0].title(prm);
-        } catch (e) {
+        try { return this.header().text(prm); } catch (e) {
             console.error(e.stack);
             throw e;
         }
 
     }
     
-    syncHeight (prm) {
-        try {
-            if (undefined === prm) {
-                /* getter */
-                return (undefined === this.m_synchei) ? false : this.m_synchei;
-            }
-            /* setter */
-            if ('boolean' !== typeof prm) {
-                throw new Error('invalid parameter');
-            }
-            this.m_synchei = prm;
-            if (true === prm) {
-                this.height(this.height());
-            }
-        } catch (e) {
-            console.error(e.stack);
-            throw e;
-        }
-    }
+    //syncHeight (prm) {
+    //    try {
+    //        if (undefined === prm) {
+    //            /* getter */
+    //            return (undefined === this.m_synchei) ? false : this.m_synchei;
+    //        }
+    //        /* setter */
+    //        if ('boolean' !== typeof prm) {
+    //            throw new Error('invalid parameter');
+    //        }
+    //        this.m_synchei = prm;
+    //        if (true === prm) {
+    //            this.height(this.height());
+    //        }
+    //    } catch (e) {
+    //        console.error(e.stack);
+    //        throw e;
+    //    }
+    //}
     
-    height (prm) {
-        try {
-            let ret = super.height(prm);
-            if ( (undefined === ret) && (true === this.syncHeight()) ) {
-                this.child()[0].height(prm);
-            }
-            return ret;
-        } catch (e) {
-            console.error(e.stack);
-            throw e;
-        }
-    }
+    //height (prm) {
+    //    try {
+    //        let ret = super.height(prm);
+    //        if ( (undefined === ret) && (true === this.syncHeight()) ) {
+    //            this.child()[0].height(prm);
+    //        }
+    //        return ret;
+    //    } catch (e) {
+    //        console.error(e.stack);
+    //        throw e;
+    //    }
+    //}
 }
 module.exports = mofron.comp.TtlFrame;
 /* end of file */

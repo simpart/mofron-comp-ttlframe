9a0e0e16630173601cb92083eb625430867dd645
simpart <simpart>
2019-01-04 10:17:53 +0000

    refactoring

--- index.js
+++ index.js
@@ -17,12 +17,12 @@ mf.comp.TtlFrame = class extends Frame {
      * 
      * @param po paramter or option
      */
-    constructor (po) {
+    constructor (po, p2) {
         try {
             super();
             this.name('TtlFrame');
-            this.prmMap('text');
-            this.prmOpt(po);
+            this.prmMap(['text', 'child']);
+            this.prmOpt(po, p2);
         } catch (e) {
             console.error(e.stack);
             throw e;
@@ -37,7 +37,7 @@ mf.comp.TtlFrame = class extends Frame {
     initDomConts () {
         try {
             super.initDomConts();
-            this.addChild( this.header() );
+            this.addChild(this.header());
         } catch (e) {
             console.error(e.stack);
             throw e;
@@ -46,67 +46,30 @@ mf.comp.TtlFrame = class extends Frame {
     
     header (prm) {
         try {
-            if (undefined === prm) {
-                /* getter */
-                if (undefined === this.m_header) {
-                    this.header(new Header({ bind  : false }));
-                }
-                return this.m_header;
+            let ret = this.innerComp('header', prm, Header);
+            if (undefined !== prm) {
+                prm.execOption({ bind : false });
             }
-            /* setter */
-            if (true !== mf.func.isInclude(prm, 'TxtHeader')) {
-                throw new Error('invalid parameter');
-            }
-            if (undefined !== this.m_header) {
-                this.updChild(this.m_header, prm);
-            }
-            this.m_header = prm;
+            return ret;
         } catch (e) {
             console.error(e.stack);
             throw e;
         }
     }
     
-    text (prm) {
-        try { return this.header().text(prm); } catch (e) {
+    text (prm, dct) {
+        try { return this.header().text(prm, dct); } catch (e) {
             console.error(e.stack);
             throw e;
         }
-
     }
     
-    //syncHeight (prm) {
-    //    try {
-    //        if (undefined === prm) {
-    //            /* getter */
-    //            return (undefined === this.m_synchei) ? false : this.m_synchei;
-    //        }
-    //        /* setter */
-    //        if ('boolean' !== typeof prm) {
-    //            throw new Error('invalid parameter');
-    //        }
-    //        this.m_synchei = prm;
-    //        if (true === prm) {
-    //            this.height(this.height());
-    //        }
-    //    } catch (e) {
-    //        console.error(e.stack);
-    //        throw e;
-    //    }
-    //}
-    
-    //height (prm) {
-    //    try {
-    //        let ret = super.height(prm);
-    //        if ( (undefined === ret) && (true === this.syncHeight()) ) {
-    //            this.child()[0].height(prm);
-    //        }
-    //        return ret;
-    //    } catch (e) {
-    //        console.error(e.stack);
-    //        throw e;
-    //    }
-    //}
+    mainColor (prm) {
+        try { return this.header().baseColor(prm); } catch (e) {
+            console.error(e.stack);
+            throw e;
+        }
+    }
 }
 module.exports = mofron.comp.TtlFrame;
 /* end of file */
